f14256f95615566544223c07ddd75e9e6fbf1b72
qwikks <84187901+qwikks@users.noreply.github.com>
2023-09-30 16:26:23 +0700

    Update shop.py

--- modules/shop.py
+++ modules/shop.py
@@ -6,46 +6,28 @@ class_name = "Shop"
 
 
 class Shop(Module):
-    prefix = "sh"
+    prefix = "issh"
 
     def __init__(self, server):
         self.server = server
-        self.commands = {"bji": self.buy_game_item,
-                         "bwr": self.buy_wedding_ring,
-                         "bsrg": self.buy_game_item}
+        
+        self.commands = {"bsi": self.buy_game_item}
 
     async def buy_game_item(self, msg, client):
         item = msg[2]["tpid"]
         cnt = msg[2]["cnt"]
-        await self.buy(client, item, count=cnt)
+        if await self.buy(client, item, count=cnt):
+            await client.send(msg[1:])
 
-    async def buy_wedding_ring(self, msg, client):
-        item = msg[2]["tpid"]
-        await self.buy(client, item)
-
-    async def buy(self, client, item, count=1, add=True):
-        if item not in self.server.game_items["game"]:
+    async def buy(self, client, item, count=1):
+        if item in self.server.food:
+            type_ = "fd"
+        elif item in self.server.med:
+            type_ = "med"
+        else:
+            await self.server._______________________________________________________________________________(client, "shop off")
             return False
-        if not self.server.game_items["game"][item]["canBuy"]:
-            return False
-        gold = self.server.game_items["game"][item]["gold"]*count
-        silver = self.server.game_items["game"][item]["silver"]*count
-        user_data = await self.server.get_user_data(client.uid)
-        if user_data["gld"] < gold or user_data["slvr"] < silver:
-            return False
-        redis = self.server.redis
-        await redis.set(f"uid:{client.uid}:gld", user_data["gld"]-gold)
-        await redis.set(f"uid:{client.uid}:slvr", user_data["slvr"]-silver)
-        await self.server.redis.incrby(f"uid:{client.uid}:exp", gold)
-        await self.server.redis.incrby(f"uid:{client.uid}:exp", round(silver / 200))
-        await refresh_avatar(client, self.server)
-        await gen_plr(client, self.server)
-        ci = await get_island_info(client.uid, self.server)
-        await client.send(["ntf.ci", {"ci": ci}])
-        if add:
-            await self.server.inv[client.uid].add_item(item, "gm", count)
-            cnt = int(await redis.lindex(f"uid:{client.uid}:items:{item}", 1))
-            await client.send(["ntf.inv", {"it": {"c": cnt, "lid": "",
-                                                  "tid": item}}])
-            await notify.update_resources(client, self.server)
+        await self.server.inv[client.uid].add_item(item, type_, count)
+        inv = self.server.inv[client.uid].get()
+        await client.send(["ntf.invch", {"inv": inv}])
         return True
